7626e3c0c89916d84d8c6893aab40a1371a6d743
hhhulkina <melissakate@mail.ru>
2017-05-21 11:41:44 -0400

    smart comment

---
+++ fill_t.sql
@@ -0,0 +1,33 @@
+
+INSERT INTO  languages (lang)
+         VALUES ('Английский');   
+INSERT INTO  languages (lang)
+         VALUES ('Испанский');
+INSERT INTO  languages (lang)
+         VALUES ('Французский');
+INSERT INTO  languages (lang)
+         VALUES ('Корейский');
+INSERT INTO  languages (lang)
+         VALUES ('Немецкий');
+
+
+INSERT INTO films (title, year)
+         VALUES ('Леон', 1994); 
+INSERT INTO films (title, year)
+         VALUES ('Жестокие игры', 1999); 
+INSERT INTO films (title, year)
+         VALUES ('Господин Никто', 2009); 
+INSERT INTO films (title, year)
+         VALUES ('Бойцовский клуб', 1999); 
+INSERT INTO films (title, year)
+         VALUES ('Титаник', 1997); 
+INSERT INTO films (title, year)
+         VALUES ('Криминальное чтиво', 1994); 
+INSERT INTO films (title, year)
+         VALUES ('Телохранитель', 1992); 
+INSERT INTO films (title, year)
+         VALUES ('Донни Дарко', 2001); 
+INSERT INTO films (title, year)
+         VALUES ('Стражи галактики', 2014);
+INSERT INTO films (title, year)
+         VALUES ('500 дней лета', 2009);
