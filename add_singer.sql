4875558005c9b3049e699794b802539796aae3d5
Firefly7618 <firefly7618@gmail.com>
2017-05-22 15:12:21 -0400

    new prodecure and tests for it

---
+++ add_singer.sql
@@ -0,0 +1,44 @@
+CREATE OR REPLACE PROCEDURE add_singer(
+	l_singer_name in Singers.name%TYPE,
+	l_singer_description in Singers.description%TYPE,
+	l_language NVARCHAR2
+)
+AS
+l_language_index Languages.id%TYPE;
+
+BEGIN
+	BEGIN
+		SELECT id INTO l_language_index
+			FROM Languages
+			WHERE lang = l_language;
+
+		INSERT INTO Singers(name, description, language_id)
+			VALUES (l_singer_name, l_singer_description, l_language_index);
+
+		dbms_output.put_line('Исполнитель добавлен!');
+
+	EXCEPTION
+		WHEN DUP_VAL_ON_INDEX THEN
+			dbms_output.put_line('Такой исполнитель существует');
+	
+		WHEN NO_DATA_FOUND THEN
+
+			BEGIN
+				INSERT INTO Languages(lang) VALUES (l_language)
+					RETURNING id INTO l_language_index;
+				INSERT INTO Singers(name, description, language_id)
+					VALUES (l_singer_name, l_singer_description, l_language_index);
+				
+				dbms_output.put_line('Исполнитель добавлен!');
+			END;
+
+	END;
+	
+	EXCEPTION
+		WHEN DUP_VAL_ON_INDEX THEN
+			BEGIN
+				ROLLBACK;
+				dbms_output.put_line('Такой исполнитель существует');
+			END;		
+END;
+/
---
+++ tests.sql
@@ -0,0 +1,36 @@
+BEGIN
+	add_singer('rolling stones', 'asdas', 'Английский');
+END;
+/
+
+commit;
+
+BEGIN
+	add_singer('beatles', 'asdas', 'Английский');
+END;
+/
+
+commit;
+
+BEGIN
+	add_singer('rolling stones', 'asdas', 'aaaaaaa');
+END;
+/
+
+commit;
+
+
+BEGIN
+	add_singer('rolling stones', 'asdas', 'Английский');
+END;
+/
+
+commit;
+
+
+BEGIN
+	add_singer('Toto Cutugno', 'asdas', 'Итальянский');
+END;
+/
+
+commit;
