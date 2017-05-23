0a2a2132de35b7510da9b8df1d9e070db13dabc1
Firefly7618 <firefly7618@gmail.com>
2017-05-23 15:25:30 -0400

    for future convenience

---
+++ create_all.sql
@@ -0,0 +1,6 @@
+@/home/oracle/lab/lab_db/create_t.sql
+@/home/oracle/lab/lab_db/resolve_film_songs.sql
+@/home/oracle/lab/lab_db/add_singer.sql
+@/home/oracle/lab/lab_db/add_song.sql
+
+commit;
---
+++ drop_all.sql
@@ -0,0 +1,5 @@
+DROP PROCEDURE add_song;
+DROP PROCEDURE add_singer;
+DROP PROCEDURE resolve_film_songs;
+@/home/oracle/lab/lab_db/drop_t.sql
+
