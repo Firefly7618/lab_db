411fe8895f331aac83f20e86b12d22460ac10638
Firefly7618 <firefly7618@gmail.com>
2017-05-24 12:51:10 -0400

    tests added

--- tests.sql
+++ tests.sql
@@ -1,27 +1,10 @@
 BEGIN
 	add_singer('rolling stones', 'asdas', 'Английский');
-END;
-/
-
-commit;
-
-BEGIN
 	add_singer('beatles', 'asdas', 'Английский');
-END;
-/
-
-commit;
-
-BEGIN
 	add_singer('rolling stones', 'asdas', 'aaaaaaa');
-END;
-/
-
-commit;
-
-
-BEGIN
 	add_singer('rolling stones', 'asdas', 'Английский');
+	add_singer('Toto Cutugno', 'asdas', 'Итальянский');
+	add_song('rolling stones', 'title', 'lyrics', 'album', 'genre','Английский', 'film');
 END;
 /
 
@@ -29,7 +12,45 @@ commit;
 
 
 BEGIN
-	add_singer('Toto Cutugno', 'asdas', 'Итальянский');
+	add_song('rolling stones', 'title', 'lyrics', 'album', 'genre','Английский', 'film');
+END;
+/
+
+commit;
+
+BEGIN
+	add_song('rolling stones', 'title2', 'lyrics', 'album', 'genre','Английский', 'film');
+END;
+/
+
+commit;
+
+
+BEGIN
+	add_song('rolling stones', 'title3', 'lyrics', 'album2', 'genre','Английский', 'film');
+END;
+/
+
+commit;
+
+
+BEGIN
+	add_song('rolling stones2', 'title3', 'lyrics', 'album2', 'genre','Английский', 'film');
+END;
+/
+
+commit;
+
+
+BEGIN
+	add_song('rolling stones', 'title4', 'lyrics', 'album2', 'genre2','Английский', 'film');
+END;
+/
+
+commit;
+
+BEGIN
+	add_song('rolling stones2', 'title4', 'lyrics', 'album2', 'genre2','Английский2', 'film');
 END;
 /
 
