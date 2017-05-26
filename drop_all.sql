db63dc0c6481290c4dcb15749db40f34fc75d14b
Firefly7618 <firefly7618@gmail.com>
2017-05-26 06:55:58 -0400

    everything is here

--- drop_all.sql
+++ drop_all.sql
@@ -1,5 +1,12 @@
 DROP PROCEDURE add_song;
 DROP PROCEDURE add_singer;
 DROP PROCEDURE resolve_film_songs;
+DROP PROCEDURE add_translation;
+DROP PROCEDURE add_user;
+DROP PROCEDURE update_album;
+DROP PROCEDURE delete_song;
+DROP PROCEDURE test_get_rating;
+DROP FUNCTION get_user_rating;
+
 @/home/oracle/lab/lab_db/drop_t.sql
 
